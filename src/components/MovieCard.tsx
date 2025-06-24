b70570b40d4220affb91431f2faffd7101190b08
Harshit Shukla <120409631+Harshitshukla0208@users.noreply.github.com>
2025-06-25 03:38:21 +0530

    fix: auto-applied AI suggestion

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -18,7 +18,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie, onClick }) => {
 
     const getEmbedUrl = (driveUrl: string): string => {
         const fileId = driveUrl.match(/\/d\/(.+?)\//)?.[1];
-        return fileId ? `https://drive.google.com/file/d/${fileId}/preview` : '';
+        if (!fileId) { console.warn('Invalid drive URL provided.'); return ''; } return `https://drive.google.com/file/d/${fileId}/preview`; 
     };
 
     const embedUrl = getEmbedUrl(movie.movieLink);
